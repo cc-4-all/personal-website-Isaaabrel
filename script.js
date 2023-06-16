a39148fc2ed003be1db6c70c1ccccdd1a7fc047b
jakewarrenblack <47800618+jakewarrenblack@users.noreply.github.com>
2023-06-16 13:57:37 +0100

    Jake fixes

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-let pic = document.getElementById("profile-picture");
+var img = document.getElementById('gallery-img')
+var prev = document.getElementById('prev')
+var next = document.getElementById('next')
 
-document.addEventListener("scroll", function () {
-  if (window.scrollY > 5) {
-    pic.style.borderRadius = "0%";
-  } else {
-    pic.style.borderRadius = "100%";
-  }
-});
+var images = ["Kitten.jpg"]
+
+var currentimage = 0;
+
+function changeimage(e) {
+    if (e.target.id == "next" && currentimage ! == images.length - 1) {
+        currentimage = currentimage + 1;
+    }
+
+    if (e.target.id == "prev" && currentimage !== images.length - 1) {
+        currentimage = currentimage - 1;
+    }
+
+    img.setAttribute("src", "images/" + images [currentimage]);
+}
+
+prev.addEventListener("click", changeimage);
+next.addEventListener("click", changeimage)
